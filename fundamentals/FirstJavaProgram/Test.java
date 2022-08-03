0506f5b304116813e28a2e8921c0e7099e3d3a96
Charlie <c.alejandro.oso@gmail.com>
2022-08-03 16:00:34 -0700

    first commit

---
+++ fundamentals/FirstJavaProgram/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is Carlos Osornio");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Anaheim, CA");
+    }
+}
---
+++ fundamentals/greetings/Test.java
@@ -0,0 +1,12 @@
+// package fundamentals.greetings;
+
+// File name must be the same as the class name
+// Every file IS a class
+public class Test {
+    // The file or class with the exact method signature below
+    // is always the launching point of the application
+    // You cannot run a Java program without it. 
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
