af5a4a611bb0620a5a18e1decc9c2ca11106ab3f
Charlie <c.alejandro.oso@gmail.com>
2022-09-04 19:25:29 -0700

    added book club assignment

---
+++ Spring/SpringProjects/login&registration/src/main/java/com/carloso/loginandregistration/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.carloso.loginandregistration.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.carloso.loginandregistration.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+	Optional<User> findById(Long id);
+	Optional<User> findByEmail(String email);
+}
