f6185630d0a0b12f3acea19732b0da3d431c254c
Carlos Osornio <104538332+c-osornio@users.noreply.github.com>
2023-05-06 00:50:55 -0700

    Update AlfredQuotes.java

--- fundamentals/AlfredBot/AlfredQuotes.java
+++ fundamentals/AlfredBot/AlfredQuotes.java
@@ -3,7 +3,7 @@ public class AlfredQuotes {
     
     public String basicGreeting() {
         return "Hello, lovely to see you. How are you?";
-    }
+    } 
     
     public String guestGreeting(String name) {
         String greetings = String.format("Hello, %s. Lovely to see you!", name);
